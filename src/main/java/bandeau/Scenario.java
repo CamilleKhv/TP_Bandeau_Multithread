48de29e55c46025394e4f24a7d7778c11dd0c758
CamilleKerherve <camille.kerherve@etud.univ-jfc.fr>
2024-10-09 09:39:29 +0200

    Lock/synchronized

--- src/main/java/bandeau/Scenario.java
+++ src/main/java/bandeau/Scenario.java
@@ -1,6 +1,10 @@
 package bandeau;
+
 import java.util.List;
 import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Classe utilitaire pour représenter la classe-association UML
@@ -15,18 +19,23 @@ class ScenarioElement {
         repeats = r;
     }
 }
+
 /**
  * Un scenario mémorise une liste d'effets, et le nombre de repetitions pour chaque effet
  * Un scenario sait se jouer sur un bandeau.
  */
-public class Scenario {
+public class Scenario extends Thread {
+
+
+    private Lock lock = new ReentrantLock();
+
 
     private final List<ScenarioElement> myElements = new LinkedList<>();
 
     /**
      * Ajouter un effect au scenario.
      *
-     * @param e l'effet à ajouter
+     * @param e       l'effet à ajouter
      * @param repeats le nombre de répétitions pour cet effet
      */
     public void addEffect(Effect e, int repeats) {
@@ -39,10 +48,16 @@ public class Scenario {
      * @param b le bandeau ou s'afficher.
      */
     public void playOn(Bandeau b) {
-        for (ScenarioElement element : myElements) {
-            for (int repeats = 0; repeats < element.repeats; repeats++) {
-                element.effect.playOn(b);
+        Thread t = new Thread(){
+            public void run () {
+                for (ScenarioElement element : myElements) {
+                    for (int repeats = 0; repeats < element.repeats; repeats++) {
+                        element.effect.playOn(b);
+                    }
+                }
             }
-        }
+        };
+        t.start();
     }
 }
+
